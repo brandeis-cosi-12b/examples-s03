ec0755d35928067f315f0046d6a8da90f5ec02e9
Joe Delfino <jdelfino@gmail.com>
2024-09-06 21:41:55 +0000

    string comparisons

---
+++ src/main/edu/brandeis/lecture3/StringDotEqualsComparisons.java
@@ -0,0 +1,18 @@
+package edu.brandeis.lecture3;
+
+import java.util.Scanner;
+
+public class StringDotEqualsComparisons {
+   public static void main(String[] args) {
+      Scanner console = new Scanner(System.in);
+      System.out.println("Guess the secret word ");
+      String guess = console.next();
+
+      if (guess.equals("cosi")) {
+         System.out.println("You got it!");
+      } else {
+         System.out.println("Sorry, try again.");
+      }
+      console.close();
+   }
+}
---
+++ src/main/edu/brandeis/lecture3/StringEqualsEqualsComparisons.java
@@ -0,0 +1,20 @@
+package edu.brandeis.lecture3;
+
+import java.util.Scanner;
+
+public class StringEqualsEqualsComparisons {
+   public static void main(String[] args) {
+      Scanner console = new Scanner(System.in);
+      System.out.println("Guess the secret word ");
+      String guess = console.next();
+
+      if (guess == "cosi") {
+         // Note: this code is incorrect. This will never print!
+         System.out.println("You got it!");
+      } else {
+         System.out.println("Sorry, try again.");
+      }
+
+      console.close();
+   }
+}
