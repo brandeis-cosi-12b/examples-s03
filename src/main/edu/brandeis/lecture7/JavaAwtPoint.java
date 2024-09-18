ef49f0d54521c2b97c58dc3f911c09d58bcfcf5b
Joe Delfino <jdelfino@gmail.com>
2024-09-18 18:42:45 +0000

    point v1

--- src/main/edu/brandeis/lecture7/JavaAwtPoint.java
+++ src/main/edu/brandeis/lecture7/JavaAwtPoint.java
@@ -2,11 +2,11 @@ package edu.brandeis.lecture7;
 
 import java.awt.Point;
 
-public class PointExample1{
+public class JavaAwtPoint{
     public static void main(String[] args){
         Point p = new Point(3, 8);
         System.out.println("initially p = " + p);
         p.translate(-1, -2);
         System.out.println("after translating p = " + p);
     }
-    }
+}
---
+++ src/main/edu/brandeis/lecture7/ver1/Point.java
@@ -0,0 +1,6 @@
+package edu.brandeis.lecture7.ver1;
+
+public class Point{
+    int x;
+    int y;
+}
