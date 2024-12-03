f5ba0723ab58b8f004300ef770f0db5fb1b533d8
Joe Delfino <jdelfino@gmail.com>
2024-12-03 20:01:33 +0000

    lotto starter

---
+++ src/main/edu/brandeis/lecture25/Lotto.java
@@ -0,0 +1,5 @@
+package edu.brandeis.lecture25;
+
+public class Lotto {
+    
+}
---
+++ src/test/edu/brandeis/lecture25/LottoTest.java
@@ -0,0 +1,12 @@
+package edu.brandeis.lecture25;
+
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+public class LottoTest {
+    @Test
+    public void testLotto() {
+        assertFalse(true);
+    }
+}
